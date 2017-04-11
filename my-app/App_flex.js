7b5992557d3755755fc2d0535ff505915109eda6
hatoriz <ninja.hatoriz@gmail.com>
2017-04-11 22:15:32 +0700

    [REACT] - flex, textinput

--- my-app/App_flex.js
+++ my-app/App_flex.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,24 +16,21 @@ const styles = StyleSheet.create({
 });
 
 export default class MyApp extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {text: ''};
-    }
-
   render() {
     return (
-      <View style={{padding: 10}}>
-
-        <TextInput
-          style={{paddingTop: 20, height: 40}}
-          placeholder="Type here!"
-          onChangeText={(text) => this.setState({text})}
-        />
-
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text}
-        </Text>
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimensions, so the children can't expand.
+      // What if you add `height: 300` instead of `flex: 1`?
+      <View style={{flex: 1}}>
+        <View style={{flex: 5}}>
+          <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+          <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+          <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+        </View>
+        <View style={{flex:1}}>
+          <View style={{flex: 1, backgroundColor: 'plum'}} />
+          <View style={{flex: 1, backgroundColor: 'peru'}} />
+        </View>
       </View>
     );
   }
